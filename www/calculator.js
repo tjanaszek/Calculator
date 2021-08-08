68e75181a874506182dbcb148dd7246ec0f8dbc5
TeresaJanaszek <teresa.janaszek@gmail.com>
2021-08-08 13:25:33 +0200

    Added files to project

---
+++ www/calculator.js
@@ -0,0 +1,12 @@
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+
+keys.addEventListener('click', event =>{
+    if(!event.target.match('button')){
+        return;
+    }
+    
+    
+
+
+})
